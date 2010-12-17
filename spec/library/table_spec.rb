07d05b5745c95cb1ffb1c5d65bf76aa0a33a6bba
Brian Ford <brixen@gmail.com>
2010-12-16 16:50:49 -0800

    Don't use 'should' in specs.

--- spec/library/table_spec.rb
+++ spec/library/table_spec.rb
@@ -1,12 +1,12 @@
 require 'spec/spec_helper'
 
 describe "Table" do
-  it "should be created by a _list_ table literal with expressions" do
+  it "is created by a sequence of values in paretheses" do
     table = Poison::CodeLoader.execute "(1, 2, 3)"
     table.should be_kind_of(Table)
   end
 
-  it "should be created by a _hash_ table literal with assignments" do
+  it "is created by sequence of assignments in parentheses" do
     table = Poison::CodeLoader.execute "(a = 1, b = 2, c = 3)"
     table.should be_kind_of(Table)
   end
