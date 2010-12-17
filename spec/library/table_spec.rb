58bdc5f33ac44c5286d191eafe17ba4d3c0d55a3
Victor Hugo Borja <vborja@apache.org>
2010-12-17 01:43:31 -0600

    Fixed indentation

--- spec/library/table_spec.rb
+++ spec/library/table_spec.rb
@@ -4,7 +4,7 @@ describe "Table" do
   it "is created by a sequence of values in paretheses" do
     table = Poison::CodeLoader.execute "(1, 2, 3)"
     table.should be_kind_of(Table)
-    end
+  end
 
   it "is created by sequence of assignments in parentheses" do
     table = Poison::CodeLoader.execute "(a = 1, b = 2, c = 3)"
