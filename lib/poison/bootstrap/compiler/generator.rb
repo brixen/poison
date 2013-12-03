eb1c1a31a500cae984776c6a0c6b2c7fb5870367
Brian Shirai <brixen@gmail.com>
2013-12-03 15:52:40 -0800

    Fixed scope of Generator.

--- lib/poison/bootstrap/compiler/generator.rb
+++ lib/poison/bootstrap/compiler/generator.rb
@@ -1,6 +1,6 @@
 module Poison
   class Compiler
-    class Generator < Rubinius::Generator
+    class Generator < Rubinius::ToolSet::Runtime::Generator
       def pn_send(selector, args=0)
         send :"pn:#{selector}", args
       end
